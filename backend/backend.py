346967620a27eb5787daa0df32d8b32ab0cc1a43
Surya Makthal <smaktha@ncsu.edu>
2021-09-15 16:52:59 -0400

    update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-import time, random
+import random
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -13,7 +13,6 @@ def ping():
 @app.route('/generate-board', methods=['GET'])
 @cross_origin()
 def generate():
-    random.seed(time.time())
     items = []
     with open('txt/cleaned_items.txt', 'r') as file:
         items_raw = file.read()
@@ -22,7 +21,4 @@ def generate():
         return jsonify([items[:5], items[5:10], items[10:15], items[15:20], items[20:25]])
 
 if __name__ == '__main__':
-    try:
-        app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
-    except:
-        print("Error running server")
+    app.run(threaded=True, port=5000)
